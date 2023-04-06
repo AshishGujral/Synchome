69c94facf8d008548b13955a40003012539539ae
preetsimon <77456490+preetsimon@users.noreply.github.com>
2023-04-06 13:34:40 -0700

    motion sensor api integrated

--- frontend/src/pages/motionControl/MotionControl.jsx
+++ frontend/src/pages/motionControl/MotionControl.jsx
@@ -1,9 +1,11 @@
+import { useState, useEffect, useContext } from "react";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Topbar from "../../components/Topbar/Topbar";
 import FourColumnDiv from "../../components/main/FourColumnDiv";
 import ChartExpense from "../../components/ChartExpense/ChartExpense";
-
-import ForestIcon from "@mui/icons-material/Forest";
+import axios from "axios";
+import { Context } from "../../context/Context";
+import TrackChangesIcon from "@mui/icons-material/TrackChanges";
 import { Grid, styled } from "@mui/material";
 
 const SidebarGrid = styled(Grid)`
@@ -22,37 +24,66 @@ const MainGrid = styled(Grid)`
   padding-left: 2vw;
 `;
 // fake data for info box
-const infoData = [{
-    deviceId:1,
-    time : 3.4,
-    accessedBy : "Amy",
+const infoData = [
+  {
+    deviceId: 1,
+    time: 3.4,
+    accessedBy: "Amy",
     waterConsumed: 2.5,
-}]
+  },
+];
 
 const MotionControl = () => {
+  const { user } = useContext(Context);
+
+  const [nameOne, setNameOne] = useState("Kitchen");
+  const [valueOne, setValueOne] = useState(false);
+
+  const switchToggleOne = async () => {
+    setValueOne(!valueOne);
+    console.log("Button clicked");
+    const status = valueOne ? "OFF" : "ON";
+
+    try {
+      await axios.post("/api/routes/manageMotion", {
+        userId: user._id,
+        status: status,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <ContainerGrid container spacing={2} justifyContent="flex-start">
-    <SidebarGrid item xs={1}>
-      <Sidebar />
-    </SidebarGrid>
-    {/* ---------------------------------------------------------- */}
-    <MainGrid item className="water__herosection" xs={8}>
-    {
-      infoData.map(({time, accessedBy, waterConsumed}) => {
+      <SidebarGrid item xs={1}>
+        <Sidebar />
+      </SidebarGrid>
+      {/* ---------------------------------------------------------- */}
+      <MainGrid item className="water__herosection" xs={8}>
+        {infoData.map(({ time, accessedBy, waterConsumed }) => {
           return (
-              <div className="water__wrapper">
+            <div className="water__wrapper">
               <div className="water__switches">
                 <FourColumnDiv
-                  switches={[{ name: "Yard", state: false, icon: <ForestIcon /> }]}
+                  switches={[
+                    {
+                      name: nameOne,
+                      state: valueOne,
+                      icon: <TrackChangesIcon />,
+                      handleChange: switchToggleOne,
+                      color: "#7a40f2",
+                    },
+                  ]}
                 />
               </div>
               <div className="water__chartInfo">
                 <div className="water__powerConsumed">
-                  <ChartExpense />
+                  {/* <ChartExpense /> */}
                 </div>
               </div>
               <div className="water__info" id="water__info">
-                <label for="water__info">Yard</label>
+                <label for="water__info">Location: </label>
                 <h4>Last triggred:</h4>
                 {/* insert data */}
                 <h5>{`${time}hrs ago`}</h5>
@@ -61,17 +92,15 @@ const MotionControl = () => {
               </div>
             </div>
           );
-      })
-    }
-    </MainGrid>
-    {/* ----------------------------------------------- */}
+        })}
+      </MainGrid>
+      {/* ----------------------------------------------- */}
 
-    <TopbarGrid className="rightColumn" item xs={3}>
-      <Topbar />
-    </TopbarGrid>
-  </ContainerGrid>
-);
-  
-}
+      <TopbarGrid className="rightColumn" item xs={3}>
+        <Topbar />
+      </TopbarGrid>
+    </ContainerGrid>
+  );
+};
 
-export default MotionControl
+export default MotionControl;
